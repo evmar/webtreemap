evmar/webtreemap

commit a994ee8ca673b07f6ad99a3ab3bf71d686be5bc2

Add an ability to set custom data on the dom elements. if custom data is provided in the data field of the node it will be set as data attribute. the name of the attribute will be 'data-{the key used in the data field}' for example: if the node obj looks like this: {     name: '/ 22.3m',     data: {         $area: '22271290',         size: '22300000'     } }
there will be additional data-size="22300000"  on the resulted element

webtreemap.js
@@ -69,6 +69,12 @@ function makeDom(tree, level) {
     dom.className += (' webtreemap-aggregate');
   }
 
+  for(key in tree.data){
+    if(key != '$area'){
+      dom.setAttribute('data-' + key, tree.data[key]);
+    }
+  }
+
   dom.onmousedown = function(e) {
     if (e.button == 0) {
       if (focused && tree == focused && focused.parent) {

commit f28163e8d77a44a1c7bc80bcd97daa6ba363c3ca

Fix $symbol and $dominant_symbol
When creating custom data attributes, have makeDom() ignore tree.data keys
that begin with $. Avoids creating an attribute with the illegal name
"data-$symbol".

Broken since commit a994ee8c:
"Add an ability to set custom data on the dom elements."

webtreemap.js
@@ -77,8 +77,8 @@ function makeDom(tree, level) {
     dom.className += (' webtreemap-aggregate');
   }
 
-  for(key in tree.data){
-    if(key != '$area'){
+  for (key in tree.data) {
+    if (!key.startsWith('$')) {
       dom.setAttribute('data-' + key, tree.data[key]);
     }
   }